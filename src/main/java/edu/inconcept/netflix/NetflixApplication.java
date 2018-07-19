e504db6a6360b97f45dc4df17dba4a47cb3d3db0
Martin <martin.topchyan@gmail.com>
2018-07-19 15:08:52 +0400

    new project name

--- src/main/java/edu/inconcept/netflix/NetflixApplication.java
+++ src/main/java/edu/inconcept/netflix/NetflixApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MovieAffinityApplication {
+public class NetflixApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MovieAffinityApplication.class, args);
+		SpringApplication.run(NetflixApplication.class, args);
 	}
 }
